1013e1abce4e22811308d9e22fe1527332c36fae
CaioSchmidt10 <caioschmidtcdc@gmail.com>
2024-06-04 17:02:35 -0300

    Estilização Bootstrap Tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AlunosComponent } from './components/alunos/alunos.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AlunosComponent
+    AlunosComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import "bootstrap-icons/font/bootstrap-icons.min.css"
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
