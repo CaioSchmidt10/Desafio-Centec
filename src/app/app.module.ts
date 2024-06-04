6bdd3282d7476bc73858226bad295aaa951df565
Caio Schmidt <caioschmidtcdc@gmail.com>
2024-06-03 22:02:04 -0300

    Inicio Projeto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AlunosComponent } from './components/alunos/alunos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlunosComponent
   ],
   imports: [
     BrowserModule,
